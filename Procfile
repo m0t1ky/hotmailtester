c5e371094cfda90cfaf7516d783d2def55ab2777
m0t1ky <106991060+m0t1ky@users.noreply.github.com>
2022-06-06 17:10:17 -0300

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hotmail:hotmail
---
+++ hotmail.py
@@ -0,0 +1,17 @@
+import imaplib
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/api/hotmail')
+def hotmail():
+	# create an IMAP4 class with SSL 
+	imap = imaplib.IMAP4_SSL("imap-mail.outlook.com")
+	# authenticate
+	try:
+  		imap.login(request.args.get("login"), request.args.get("passwd"))
+  		return '{"status": "success"}'
+	except:
+  		return '{"status": "failed"}'
